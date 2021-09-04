ffc19eb7d6134c195063377809f1f62203995abc
Aadhith-Ujesh <ujeshm2k2@gmail.com>
2021-09-04 19:29:31 +0530

    tailwind css init

--- realtime-quizer/src/login.js
+++ realtime-quizer/src/login.js
@@ -1,9 +1,12 @@
 import './login.css'
+// import './index.css'
 
 function login(){
    return(
-        <div class="login">
-            <input type = "text" name = "username"/>
+        <div className="login bg-gray-200">
+            <label className="max-w-sm rounded overflow-hidden shadow-lg">Enter username</label>
+            <input type = "text" name = "username"/><br/>
+            <label>Enter password</label>
             <input type = "password" name = "password"/>
         </div>
    );   
