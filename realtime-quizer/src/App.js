7ed00913958a1d129204a98e6c399ff23a688f50
Aadhith-Ujesh <ujeshm2k2@gmail.com>
2021-09-04 19:06:34 +0530

    init
    Co-authored-by: John-Tenning <John-Tenning@users.noreply.github.com>

---
+++ realtime-quizer/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import Login from './login';
+import { Route, Switch } from 'react-router-dom';
+
+function App() {
+  return (
+    <div className="App">
+              <Switch> 
+               <Route exact path="/">
+                  <Login/>
+                </Route>
+            
+              </Switch>
+    </div>
+  );
+}
+
+export default App;
---
+++ realtime-quizer/src/login.js
@@ -0,0 +1,12 @@
+import './login.css'
+
+function login(){
+   return(
+        <div class="login">
+            <input type = "text" name = "username"/>
+            <input type = "password" name = "password"/>
+        </div>
+   );   
+}
+
+export default login
